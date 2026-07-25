9d079aeb2e5e1fe412e99a1d60c6bc8feb583f3c
Erik Andreasson <erikandreasson@Mac.lan>
2026-07-25 18:24:30 +0200

    Nu kan man köra programmet med routes

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -2,14 +2,15 @@ from pathlib import Path
 from fastapi import APIRouter, FastAPI
 
 # --- Dynamic folder-finding ---
-APP_DIR = Path(__file__).resolve().parent.parent.parent
+APP_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = APP_DIR / "client" / "templates"
 
 app = FastAPI()
 router = APIRouter()
 
 # ======== Page Routers ========
-router.frontend("/login", directory=str(TEMPLATES_DIR / "login.html"))
-router.frontend("/dashboard", directory=str(TEMPLATES_DIR / "dashboard.html"))
+router.frontend("/", directory=str(TEMPLATES_DIR))
+router.frontend("/login", directory=str(TEMPLATES_DIR))
+router.frontend("/dashboard", directory=str(TEMPLATES_DIR), fallback="index.html")
 
 app.include_router(router)
