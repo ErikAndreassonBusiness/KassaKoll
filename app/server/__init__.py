cbf62f0e3b0a48410ffa3623396a8b4cec5c0eec
Erik Andreasson <erikandreasson@MacBook-Air-som-tillhor-Erik.local>
2026-07-27 13:00:13 +0200

    bra

--- app/server/__init__.py
+++ app/server/__init__.py
@@ -1,20 +1,5 @@
-from pathlib import Path
-from fastapi import FastAPI
-
 # Responsibility: Create and expose the app instance
-
-# --- Dynamic folder-finding ---
-APP_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR = APP_DIR / "client" / "templates"
+from fastapi import FastAPI
 
 app = FastAPI(title="KassaKoll API")
 
-# # ======== Page Routers ========
-app.frontend("/", directory=str(TEMPLATES_DIR))
-
-
-# router.frontend("/", directory=str(TEMPLATES_DIR))
-# router.frontend("/login", directory=str(TEMPLATES_DIR))
-# router.frontend("/dashboard", directory=str(TEMPLATES_DIR), fallback="index.html")
-
-# app.include_router(router)
